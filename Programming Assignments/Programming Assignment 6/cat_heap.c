29d9dada0e6934909771f51c82d740ec227585b2
Spiderman103 <sahil.narsinghani14@gmail.com>
2026-04-21 14:09:30 -0400

    PG6 almost there. Fixing syntax issues with test cases when testing.

--- Programming Assignments/Programming Assignment 6/cat_heap.c
+++ Programming Assignments/Programming Assignment 6/cat_heap.c
@@ -106,6 +106,7 @@ int main() {
         } else if (strcmp(command, "REMOVE") == 0) {
             char name[MAX_NAME];
             scanf("%s", name);
+            cmd_remove(&s, name);
         } else if (strcmp(command, "PEEK") == 0) {
             cmd_peek(&s);
         } else if (strcmp(command, "SERVE") == 0) {
@@ -137,47 +138,47 @@ int main() {
 }
 
 void swapCats(Cat *a, Cat *b) {
-    Cat temp = *a;
-    *a = *b;
-    *b = temp;
+  Cat temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
-int better_cat(CatHeap *heap, Cat *left, Cat *right) {
-    if (heap->mode == MODE_ADOPTION) {
-        if (left->key > right->key + EPSILON) {
-            return 1;
-        }
-        if (right->key > left->key + EPSILON) {
-            return 0;
-        }
+int betterCat(CatHeap *heap, Cat *left, Cat *right) {
+  if (heap->mode == MODE_ADOPTION) {
+    if (left->key > right->key + EPSILON) {
+      return 1;
+    }
+    if (right->key > left->key + EPSILON) {
+      return 0;
+    }
+  } else {
+    if (left->key < right->key - EPSILON) {
+      return 1;
+    }
+    if (right->key < left->key - EPSILON) {
+      return 0;
+    }
+  }
+  int cmpName = strcmp(left->name, right->name);
+  if (cmpName != 0) {
+    if (cmpName < 0) {
+      return 1;
     } else {
-        if (left->key < right->key - EPSILON) {
-            return 1;
-        }
-        if (right->key < left->key - EPSILON) {
-            return 0;
-        }
+      return 0;
     }
-    int cmpName = strcmp(left->name, right->name);
-    if (cmpName != 0) {
-        if (cmpName < 0) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+  }
 
-    if (left->arrival_id < right->arrival_id) {
-        return 1;
-    } else {
-        return 0;
-    }
+  if (left->arrival_id < right->arrival_id) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
-void percolate_up(CatHeap *heap, int index) {
+void percolateUp(CatHeap *heap, int index) {
     while (index > 0) {
         int parent = (index - 1) / 2;
-        if (better_cat(heap, &heap->arr[index], &heap->arr[parent])) {
+        if (betterCat(heap, &heap->arr[index], &heap->arr[parent])) {
             swapCats(&heap->arr[index], &heap->arr[parent]);
             index = parent;
         } else {
@@ -191,10 +192,10 @@ void heapify(CatHeap *heap, int index) {
     int right = 2 * index + 2;
     int bestOne = index;
 
-    if (left < heap->size && better_cat(heap, &heap->arr[left], &heap->arr[bestOne])) {
+    if (left < heap->size && betterCat(heap, &heap->arr[left], &heap->arr[bestOne])) {
         bestOne = left;
     }
-    if (right < heap->size && better_cat(heap, &heap->arr[right], &heap->arr[bestOne])) {
+    if (right < heap->size && betterCat(heap, &heap->arr[right], &heap->arr[bestOne])) {
         bestOne = right;
     }
 
@@ -204,6 +205,279 @@ void heapify(CatHeap *heap, int index) {
     }
 }
 
-void cmd_add(Shelter *s, char *name, char *breed, int age, int friend1, int health) {
+void freeCat(Cat *c) {
+  if (c) {
+    free(c->name);
+    free(c->breed);
+  }
+}
+
+int find_cat_index(CatHeap *heap, char *name) {
+    for (int i = 0; i < heap->size; ++i) {
+        if (strcmp(heap->arr[i].name, name) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+double compute_adoption_key(Cat *c, Shelter *s) {
+    double base = (1.6 * c->friendliness) + (1.1 * c->health) - (0.7 * c->age);
+    double factor = 0.0;
+
+    if (s->featured_breed && strcmp(c->breed, s->featured_breed) == 0) {
+        factor = s->alpha;
+    } else {
+        factor = 1.0;
+    }
     
+    return (base * factor) - (0.000001 * c->arrival_id);
+}
+
+double compute_triage_key(Cat *c) {
+    int penalty = 0;
+    if (c->age > 12) {
+        penalty = c->age - 12;
+    } else {
+        penalty = 0;
+    }
+
+    return (100 - c->health) * (2.0 + penalty * 1.0 - 0.05 * c->friendliness);
+}
+
+void recompute_all_keys_and_build(Shelter *s) {
+    for (int i = 0; i < s->heap.size; ++i) {
+        if (s->mode == MODE_ADOPTION) {
+            s->heap.arr[i].key = compute_adoption_key(&s->heap.arr[i], s);
+        } else {
+            s->heap.arr[i].key = compute_triage_key(&s->heap.arr[i]);
+        }
+    }
+
+    for (int i = s->heap.size / 2 - 1; i >= 0; --i) {
+        heapify(&s->heap, i);
+    }
+}
+
+void cmd_add(Shelter *s, char *name, char *breed, int age, int friend1, int health) {
+    if (find_cat_index(&s->heap, name) != -1) {
+        printf("Name %s already exists.\n", name);
+        return;
+    }
+
+    if (s->heap.size >= s->heap.capacity) {
+        s->heap.capacity = s->heap.capacity * 2;
+        s->heap.arr = (Cat *)realloc(s->heap.arr, s->heap.capacity * sizeof(Cat));
+    }
+
+    Cat *newCat = &s->heap.arr[s->heap.size];
+    newCat->name = (char *)malloc((strlen(name) + 1) * sizeof(char));
+    strcpy(newCat->name, name);
+
+    newCat->breed = (char *)malloc((strlen(breed) + 1) * sizeof(char));
+    strcpy(newCat->breed, breed);
+
+    newCat->age = age;
+    newCat->friendliness = friend1;
+    newCat->health = health;
+
+    ++s->next_arrival_id;
+    newCat->arrival_id = s->next_arrival_id;
+    newCat->quarantine = 0;
+
+    if (s->mode == MODE_ADOPTION) {
+        newCat->key = compute_adoption_key(newCat, s);
+    } else {
+        newCat->key = compute_triage_key(newCat);
+    }
+
+    ++s->heap.size;
+    percolateUp(&s->heap, s->heap.size - 1);
+    printf("Added %s.\n", name);
+}
+
+
+void cmd_update(Shelter *s, char *name, char *field, int new_value) {
+    int index = find_cat_index(&s->heap, name);
+    if (index == -1) {
+        printf("Cat %s not found.\n", name);
+        return;
+    }
+
+    Cat *c = &s->heap.arr[index];
+    if (strcmp(field, "QUARANTINE") == 0) {
+        c->quarantine = new_value;
+        printf("Updated %s: QUARANTINE=%d.\n", name, new_value);
+    } else {
+        if (strcmp(field, "AGE") == 0) {
+            c->age = new_value;
+        } else if (strcmp(field, "FRIEND") == 0) {
+            c->friendliness = new_value;
+        } else if (strcmp(field, "HEALTH") == 0) {
+            c->health = new_value;
+        }
+
+        if (s->mode == MODE_ADOPTION) {
+            c->key = compute_adoption_key(c, s);
+        } else {
+            c->key = compute_triage_key(c);
+        }
+
+        printf("Updated %s: %s = %d. Priority Adjusted.\n", name, field, new_value);
+    }
+    percolateUp(&s->heap, index);
+    heapify(&s->heap, index);
+}
+
+void cmd_remove(Shelter *s, char *name) {
+    int index = find_cat_index(&s->heap, name);
+    if (index == -1) {
+        printf("Cat %s not found.\n", name);
+        return;
+    }
+
+    freeCat(&s->heap.arr[index]);
+    printf("Removed %s.\n", name);
+
+    s->heap.arr[index] = s->heap.arr[s->heap.size - 1];
+    --s->heap.size;
+
+    if (index < s->heap.size) {
+        percolateUp(&s->heap, index);
+        heapify(&s->heap, index);
+    }
+}
+
+void cmd_peek(Shelter *s) {
+    if (s->heap.size == 0) {
+        printf("No cats available.\n");
+        return;
+    }
+
+    Cat *c = &s->heap.arr[0];
+    char *modeStr;
+
+    if (s->mode == MODE_ADOPTION) {
+        modeStr = "ADOPTION";
+    } else {
+        modeStr = "TRIAGE";
+    }
+
+    printf("Top[%s]: [%s] (key=%.2lf, name=%s, breed=%s, age=%d, friend=%d, health=%d)\n", modeStr, modeStr, c->key, c->name, c->breed, c->age, c->friendliness, c->health);
+}
+
+void cmd_serve(Shelter *s) {
+    if (s->heap.size == 0) {
+        printf("No adoptable cats available.\n"); 
+        return;
+    }
+
+    if (s->mode == MODE_TRIAGE) {
+        Cat topCat = s->heap.arr[0];
+        printf("Serve now: %s (key=%.2lf, name=%s, breed=%s, age=%d, friend=%d, health=%d)\n", topCat.name, topCat.key, topCat.name, topCat.breed, topCat.age, topCat.friendliness, topCat.health);
+        cmd_remove(s, topCat.name);
+    } else {
+        Cat *tempCatList = (Cat *)malloc(s->heap.size * sizeof(Cat));
+        int tempCatListCount = 0;
+        Cat servedCat;
+        int found = 0;
+
+        while (s->heap.size > 0) {
+            Cat topCat = s->heap.arr[0];
+            s->heap.arr[0] = s->heap.arr[s->heap.size - 1];
+            --s->heap.size;
+            if (s->heap.size > 0) {
+                heapify(&s->heap, 0);
+            }
+            
+            if (topCat.quarantine == 0) {
+                servedCat = topCat;
+                found = 1;
+                break;
+            } else {
+                tempCatList[tempCatListCount] = topCat;
+                ++tempCatListCount;
+            }
+        }
+        for (int i = 0; i < tempCatListCount; ++i) {
+            s->heap.arr[s->heap.size] = tempCatList[i];
+            ++s->heap.size;
+            percolateUp(&s->heap, s->heap.size - 1);
+        }
+        free(tempCatList);
+        if (found) {
+            printf("Serve now: %s (key=%.2lf, name=%s, breed=%s, age=%d, friend=%d, health=%d)\n", servedCat.name, servedCat.key, servedCat.name, servedCat.breed, servedCat.age, servedCat.friendliness, servedCat.health);
+            freeCat(&servedCat);
+        } else {
+            printf("No adoptable cats available.\n");
+        }
+    }
+}
+
+void cmd_mode(Shelter *s, char *mode_str) {
+    if (strcmp(mode_str, "ADOPTION") == 0) {
+        s->mode = MODE_ADOPTION;
+    } else if (strcmp(mode_str, "TRIAGE") == 0) {
+        s->mode = MODE_TRIAGE;
+    }
+
+    s->heap.mode = s->mode;
+    printf("Mode set to %s. Rebuilding priorities...\n", mode_str);
+    recompute_all_keys_and_build(s);
+}
+
+void cmd_featured(Shelter *s, char *breed, double alpha) {
+    free(s->featured_breed);
+    if (strcmp(breed, "NONE") == 0) {
+        s->featured_breed = NULL;
+        s->alpha = 1.0;
+        printf("Featured breed cleared. Rebuilding priorities...\n");
+    } else {
+        s->featured_breed = (char *)malloc((strlen(breed) + 1) * sizeof(char));
+        strcpy(s->featured_breed, breed);
+        s->alpha = alpha;
+        printf("Featured breed set to %s with alpha=%.2lf. Rebuilding priorities...\n", breed, alpha);
+    }
+    recompute_all_keys_and_build(s);
+}
+
+void cmd_print(Shelter *s, int k) {
+    if (s->heap.size == 0) {
+        printf("No cats available.\n");
+        return;
+    }
+
+    CatHeap tempHeap;
+    tempHeap.capacity = s->heap.size;
+    tempHeap.size = s->heap.size;
+    tempHeap.mode = s->heap.mode;
+    tempHeap.arr = (Cat *)malloc(tempHeap.size * sizeof(Cat));
+    for (int i = 0; i < s->heap.size; ++i) {
+        tempHeap.arr[i] = s->heap.arr[i];
+    }
+
+    int count = 0;
+
+    if (k < tempHeap.size) {
+        count = k;
+    } else {
+        count = tempHeap.size;
+    }
+
+    char *modeStr;
+    if (s->mode == MODE_ADOPTION) {
+        modeStr = "ADOPTION";
+    } else {
+        modeStr = "TRIAGE";
+    }
+
+    for (int i = 0; i < count; ++i) {
+        Cat topCat = tempHeap.arr[0];
+        printf("[%d] %s (key=%.2lf, %s)\n", i + 1, topCat.name, topCat.key, modeStr);
+
+        tempHeap.arr[0] = tempHeap.arr[tempHeap.size - 1];
+        --tempHeap.size;
+        heapify(&tempHeap, 0);
+    }
+    free(tempHeap.arr);
 }
